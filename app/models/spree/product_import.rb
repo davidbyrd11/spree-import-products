e420e0c5eb754bebce53588f37fa46ce1f26c10b
David Byrd <david@byrdhou.se>
2012-06-27 18:30:29 -0400

    fixes type error

--- app/models/spree/product_import.rb
+++ app/models/spree/product_import.rb
@@ -224,10 +224,10 @@ module Spree
       # What this does is only assigns values to products if the product accepts that field.
       params_hash[:price] ||= params_hash[:master_price]
       params_hash.each do |field, value|
-        if product.respond_to?("#{field}=") && field != "label"
+        if field != "label" && product.respond_to?("#{field}=") 
           product.send("#{field}=", value)
         elsif property = Spree::Property.where(["lower(name) = ?", field.tr('_',' ')]).first
-          product.product_properties.build :value => value, :property => Spree::Property.where("lower(name) = ?", property.tr('_', ' ')).first
+          product.product_properties.build :value => value, :property => property
         end
       end
 
